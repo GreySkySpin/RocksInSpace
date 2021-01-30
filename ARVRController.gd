b2f68c6cc07bf0a01a7618e28433bef06249fbf2
GreySkySpin <42599782+GreySkySpin@users.noreply.github.com>
2021-01-30 23:43:43 +0200

    arvr origin, camera and controller

--- ARVRController.gd
+++ ARVRController.gd
@@ -1,18 +1,15 @@
-extends Node
+extends ARVRController
 
 
-onready var lmao = preload("res://TestLmao.tscn")
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	instance_create(lmao) #test
 	pass # Replace with function body.
 
-func instance_create(scene):
-	var id = scene.instance()
-	add_child(id)
-	#id.position = Vector2(x, y)
-	return id
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
